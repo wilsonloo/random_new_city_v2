93543005daaa5db9d4d78060bcbc53c998d046f2
wilson <wilson@miyou.ubuntu>
2023-07-27 01:43:11 +0800

    draw region borders

--- map.lua
+++ map.lua
@@ -43,6 +43,7 @@ local function add_node(region, nx, ny, nw, nh)
         w = nw,
         h = nh,
     }
+    region.size = region.size + nw*nh
 end
 
 local function create_region(x, y, w, h)
@@ -78,23 +79,23 @@ local function split_region(region)
         local halfy = region.y + halfh
         if nx < halfx and ny < halfy then
             local rbx = mmin(nx+nw, halfx)
-            local rby = mmin(nx+nh, halfy) 
+            local rby = mmin(ny+nh, halfy) 
             add_node(region_list[RID_LT], nx, ny, rbx-nx, rby-ny)
         end
         if nx+nw >= halfx and ny < halfy then
             local lbx = mmax(nx, halfx)
-            local lby = mmin(ny, halfy)
-            add_node(region_list[RID_RT], lbx, ny, nx-lbx, lby-ny)
+            local lby = mmin(ny+nh, halfy)
+            add_node(region_list[RID_RT], lbx, ny, nx+nw-lbx, lby-ny)
         end
         if nx < halfx and ny+nh >= halfy then
             local rtx = mmin(nx+nw, halfx)
             local rty = mmax(ny, halfy)
-            add_node(region_list[RID_LB], nx, ny+nh, rtx-nx, ny+nh-rty)
+            add_node(region_list[RID_LB], nx, rty, rtx-nx, ny+nh-rty)
         end
         if nx+nx >= halfx and ny+nh >= halfy then
             local ltx = mmax(nx, halfx)
             local lty = mmax(ny, halfy)
-            add_node(region_list[RID_RB], ltx, lty, nx+nx-ltx, ny+nh-lty)
+            add_node(region_list[RID_RB], ltx, lty, nx+nw-ltx, ny+nh-lty)
         end
     end
     return region_list
@@ -139,6 +140,7 @@ local random
 local function random_amound_list(region_list, cell_w, cell_h)
     local cell_size = cell_w*cell_h
     local list = sort_regions(region_list)
+    PrintR.print_r("sorted:", list)
     for _, region in ipairs(list) do
         local node = random(region, cell_w, cell_h)
         if node ~= nil then
@@ -153,18 +155,21 @@ random = function(region, cell_w, cell_h)
     assert(region ~= nil)
     local cell_size = cell_w*cell_h
     if region.list then
+        PrintR.print_r("random amound list", region.list)
         return random_amound_list(region.list, cell_w, cell_h)
     elseif region.node then
         local remain = region.cap - region.size
         if remain < cell_size then
             return
         end
+        print("split region:", region.rid)
         region.list = split_region(region)
         region.node = nil
         return random(region, cell_w, cell_h)
     else
         -- add first node
-        local cell_x, cell_y = rand_cell(0, 0, region.w, region.h, cell_w, cell_h)
+        print("add first node")
+        local cell_x, cell_y = rand_cell(region.x, region.y, region.w, region.h, cell_w, cell_h)
         assert(cell_x)
         assert(cell_y)
         local node = {
--- test.lua
+++ test.lua
@@ -8,12 +8,35 @@ local outdir = ...
 
 local W = 200
 local H = 200
+
+local REGION_BORDER_COLOR = {r=255, g=0, b=0}
 local exp = Exporter.new(W, H, "exp")
+
+local exported_regions = {}
+local function do_export_region_border(region)
+    if not exported_regions[region.rid] then
+        exp:rect_color(REGION_BORDER_COLOR, region.x, region.y, region.w, region.h)
+        exported_regions[region.rid] = true
+        print("region border:", region.rid)
+    end
+end
+local function export_region_borders(region)
+    do_export_region_border(region)
+
+    if region.list then
+        for _, r in pairs(region.list) do
+            export_region_borders(r)
+        end
+    end
+end
 
 local map = Map.new(W, H)
 for k = 1, 10 do
     local node = map:random(40, 40)
     PrintR.print_r(k, node)
     exp:rect(node.x, node.y, node.w, node.h)
+
+    PrintR.print_r("map:", map)
+    export_region_borders(map.region)
     exp:write(outdir.."/"..k..".json")
 end
